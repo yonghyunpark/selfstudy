2e0181d4eb58a6be142cc1ac8f88b3bc7b135bb1
Yong Hyun <pyh4616@naver.com>
2023-06-18 23:25:34 +0900

    web

---
+++ src/test/java/com/selfstudy/controller/IndexControllerTest.java
@@ -0,0 +1,29 @@
+package com.selfstudy.controller;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+public class IndexControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void 메인페이지() {
+        //when
+        String body = this.restTemplate.getForObject("/", String.class);
+
+        //then
+        Assertions.assertThat(body).contains("Self Study");
+    }
+
+}
